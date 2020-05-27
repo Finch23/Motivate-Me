13e4dede7a1b35cf911afa6e1e3e231e159719ff
Abdalla Ibrahim <new@SandKIDmegasMBP.attlocal.net>
2020-05-27 19:37:52 -0400

    added controllers and routes

---
+++ server/routes/api/index.js
@@ -0,0 +1,9 @@
+const router = require ('express').Router();
+const goalRoutes = require ('./goal');
+const quotesRoutes = require ('/quotes');
+
+
+router.use('/goal', goalRoutes);
+router.use('/quotes', quotesRoutes);
+
+module.exports = router;
