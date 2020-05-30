9efe1b54709415d7967bca559853648880a0dae3
Abdalla Ibrahim <new@SandKIDmegasMBP.attlocal.net>
2020-05-29 20:37:18 -0400

    minor fixes

--- server/routes/api/quotes.js
+++ server/routes/api/quotes.js
@@ -3,7 +3,9 @@ const quotesController = require ('../../controllers/quotesController');
 
 router.route('/')
     .post(quotesController.create);
+
 router.route('/username/:username').get(quotesController.findAll);
+
 router
 .route('/:id')
 .get(quotesController.findById)
