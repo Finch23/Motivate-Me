f40e40eaa02962594700547fe9f94a80267d36a4
Jasmin Rowdy <jrowdy@wcpss.net>
2020-06-03 11:26:41 -0400

    logo, new tab for goals, min height fix

--- client/src/components/goalsList.js
+++ client/src/components/goalsList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { List, ListItem, ListItemContent, ListItemAction, Icon } from 'react-mdl';
 import '../pages/Profile/Profile.css';
 // import Motivate from '../pages/Motivate/motivate';
@@ -8,23 +7,12 @@ class GoalsList extends Component {
         
         return(
             <div className="goalsList">
-                <h3 className="DevName" align="center">My Goals</h3>
+            
+                <h3 className="DevName" align="center">Get Motivated</h3> 
                 <hr></hr>
-            <List>
-                <ListItem twoLine>
-                    <ListItemContent avatar="" >Look for a new job</ListItemContent>
-                    <ListItemAction info="">
-                    <a href="google.com"><Icon name="X" /></a>
-                    </ListItemAction>
-                </ListItem>
                 
-
-                <div>
-                <form method="get" action="/addquote">
-                    <button className="button" style={{float: "right", marginRight: "60px"}} type="submit">Add New Goal</button>
-                </form>
-                 </div>
-            </List>
+    
+                
             </div>
 
         )
