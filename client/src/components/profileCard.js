59ca0a61d0a74de94aceaff320f44a508560814a
Christian Sanchez <cas81695@gmail.com>
2020-06-04 19:23:35 -0400

    added code for react avatar to display in porfile card and added props in porfile, and storage in back end file for user

--- client/src/components/profileCard.js
+++ client/src/components/profileCard.js
@@ -1,6 +1,7 @@
 import React, { Fragment, Component } from 'react';
 import '../pages/Profile/Profile.css';
-import { createAvatarComponent, GithubSource } from 'react-avatar';
+import createAvatarComponent from 'react-avatar/es/avatar';
+import GithubSource from 'react-avatar/es/sources/Github';
 
 const Avatar = createAvatarComponent({
     sources: [ GithubSource ]
@@ -8,6 +9,7 @@ const Avatar = createAvatarComponent({
 
 class ProfileCard extends Component {
     render() {
+        console.log(this.props.user);
         
         return(
             <div className="profile-grid" class="row">
@@ -17,25 +19,13 @@ class ProfileCard extends Component {
                             <i class="message fas fa-envelope"></i>
                             <i class="notif fas fa-bell"></i>
                         <div class="pic">
-                           <Avatar githubHandle={this.state.user.githubHandle} size={150} round="20px" />
+                           <Avatar githubHandle={this.props.user.githubHandle} size={150} round="20px" />
                         </div>
                         <div class="name">Profile</div>
                         <div class="tag">@github</div>
                         </div>
                 
                         <div class="bottom-section">
-                        <div class="social-media">
-                            <a href="https://www.google.com"><i class="material-icons">add</i></a>
-                            <a href="https://www.google.com"><i class="material-icons">list</i></a>
-                            <a href="https://www.google.com"><i class="material-icons">done_all</i></a>
-                        </div>
-                
-                            <div class="videos">90<span>Add Goals!</span></div>
-                            <div class="border"></div>
-                            <div class="subscribers">204<span>Completed</span></div>
-                            <div class="border"></div>
-                            <div class="views">7.3M <span>Completed</span></div>
-
                                 {/* Bottom, white section of the profile card */}
                                 <div class="bottom-section">
                                 <   h3 align="center" className="DevName">What are you motivated to do today?</h3>
